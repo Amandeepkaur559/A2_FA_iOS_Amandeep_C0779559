ffadb421557582860e503ee79a735dd23a38ff7e
Amandeepkaur559 <Amandeepkaur559@gmail.com>
2021-02-01 07:45:08 -0500

    Initial Commit

---
+++ A2_FA_iOS_Amandeep_C0779559/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_iOS_Amandeep_C0779559
+//
+//  Created by Mac on 01/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
